0af4b22600ba187fa4b1e34a0f3d69adcd3637b8
DESKTOP-4F5D5P1\Paulo Peixoto <aluno26097@ipt.pt>
2024-06-23 22:41:01 +0100

    Fixed load balancers shells

--- scripts/setup_keepalived_backup.sh
+++ scripts/setup_keepalived_backup.sh
@@ -2,19 +2,13 @@
 
 cat <<EOF > /etc/keepalived/keepalived.conf
 vrrp_instance VI_1 {
-    state BACKUP
-    interface eth1
-    virtual_router_id 51
-    priority 100
-    advert_int 1
-    authentication {
-        auth_type PASS
-        auth_pass 1234
-    }
+    state BACKUP;
+    interface eth1;
+    virtual_router_id 51;
+    priority 100;
+    advert_int 1;
     virtual_ipaddress {
-        192.168.50.100
+        192.168.50.100;
     }
 }
 EOF
-
-systemctl restart keepalived
--- scripts/setup_loadbalancer.sh
+++ scripts/setup_loadbalancer.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# Update package lists
 apt-get update
+
+# Install Nginx and Keepalived
 apt-get install -y nginx keepalived
+
+# Enable services
 systemctl enable nginx
 systemctl enable keepalived
 
+# create a keepalived configuration file
+touch /etc/keepalived/keepalived.conf
+
+# Copy the Nginx configuration file
 cp /vagrant/scripts/nginx.conf /etc/nginx/nginx.conf
+
+# Restart Nginx
 systemctl restart nginx
