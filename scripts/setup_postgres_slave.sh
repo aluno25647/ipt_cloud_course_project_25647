a1fd534496fa5a3c10a628ad0904ea9cd5520ea7
DESKTOP-4F5D5P1\Paulo Peixoto <aluno26097@ipt.pt>
2024-06-23 21:00:26 +0100

    Corrected shell scripts

--- scripts/setup_postgres_slave.sh
+++ scripts/setup_postgres_slave.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 MASTER_IP=$1
+MSG_COLOR="\033[41m"
 
-# Instalar PostgreSQL #versao 12 cuz ubuntu-20.04
+echo -e "$MSG_COLOR$(hostname): Update package lists\033[0m"
 sudo apt-get update
-sudo apt-get install -y postgresql postgresql-contrib
 
-# Parar o serviço PostgreSQL
+echo -e "$MSG_COLOR$(hostname): Install PostgreSQL 14 and its PHP extension\033[0m"
+# Install specific version of PostgreSQL
+sudo apt-get install -y postgresql-14 php-pgsql
+
+echo -e "$MSG_COLOR$(hostname): Stop PostgreSQL service\033[0m"
 sudo systemctl stop postgresql
 
-# Fazer o backup do master
-sudo -u postgres bash -c "pg_basebackup -h $MASTER_IP -D /var/lib/postgresql/12/main -U postgres -v -P --wal-method=stream"
+echo -e "$MSG_COLOR$(hostname): Perform base backup from master\033[0m"
+sudo -u postgres pg_basebackup -h $MASTER_IP -D /var/lib/postgresql/14/main -U postgres -v -P --wal-method=stream
 
-# Configurar PostgreSQL replicação
-sudo bash -c "cat > /var/lib/postgresql/12/main/recovery.conf <<EOF
+echo -e "$MSG_COLOR$(hostname): Configure replication settings\033[0m"
+sudo bash -c "cat > /var/lib/postgresql/14/main/recovery.conf <<EOF
 standby_mode = 'on'
 primary_conninfo = 'host=$MASTER_IP port=5432 user=postgres password=password'
 trigger_file = '/tmp/postgresql.trigger.5432'
 EOF"
 
-# postgresql.conf
-sudo bash -c "cat >> /etc/postgresql/12/main/postgresql.conf <<EOF
+sudo bash -c "cat >> /etc/postgresql/14/main/postgresql.conf <<EOF
 hot_standby = on
 EOF"
 
+echo -e "$MSG_COLOR$(hostname): Start PostgreSQL service\033[0m"
 sudo systemctl start postgresql
+
+# Change back to the previous folder
+cd -
